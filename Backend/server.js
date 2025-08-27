eaac808c4798bf185d78ec8e6b4c2929c5dd3a86
Yashpithwa <153920694+Yashpithwa@users.noreply.github.com>
2025-08-27 13:03:56 +0530

    server.js

--- Backend/server.js
+++ Backend/server.js
@@ -36,5 +36,5 @@ app.use(ai);
 app.use("/api/compare", compareRoutes);
 app.use("/api",profile);
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
